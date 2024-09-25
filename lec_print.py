f49f682fc0d571805939ed28ae1e8fe5b46565e0
Romazio101 <romazio1213@gmail.com>
2024-09-25 16:06:56 +0000

    Сделал одно задание

--- lec_print.py
+++ lec_print.py
@@ -1,1 +1,11 @@
 print('Fire in the holl')
+
+print
+
+print()
+
+print(3)
+
+print(3+4)
+
+print(3, 4)
---
+++ task_1.py
@@ -0,0 +1,10 @@
+print('Hello, World!')
+
+print('Маша + Петя = Любовь')
+
+print('x = 3 + 4')
+
+a = 3
+b = 4
+x = a + b
+print('x'=3+4)
