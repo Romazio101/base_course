c67da93bece7891c54782adb8c902eb70016f415
Romazio101 <romazio1213@gmail.com>
2024-10-01 18:32:10 +0000

    Сделал домашнее задание

---
+++ task_6.py
@@ -0,0 +1,13 @@
+l = []
+
+l.append(int(input('Введите возраст')))
+
+l.append(input('введите пол'))
+
+l.append(input('введите имя'))
+
+l.append(input('Введите город'))
+
+l.append(int(input('Введите класс')))
+
+print(l)
